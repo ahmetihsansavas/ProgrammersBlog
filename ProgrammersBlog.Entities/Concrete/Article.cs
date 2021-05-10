c8deece897c3c295b1cacd25ac6f5b3f73755226
Ahmet İhsan <ahmetihsan24@gmail.com>
2021-05-10 10:38:17 +0300

    Proje dosyası ekle.

---
+++ ProgrammersBlog.Entities/Concrete/Article.cs
@@ -0,0 +1,28 @@
+﻿using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Concrete
+{
+    public  class Article:EntityBase,IEntity
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Thumbnail { get; set; } //Resimleri saklamak için 1.jpg vb.
+        public DateTime Date { get; set; }
+        public int ViewsCount { get; set; }
+        public int CommentCount { get; set; }
+        public string SeoAuthor { get; set; }
+        public string SeoDescription { get; set; }
+        public string SeoTags { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; } //Bir Makelenin bir tane Kategorisi var 
+        public int UserId { get; set; }
+        public User User { get; set; } //Bir Makelenin bir tane User'ı var 
+
+
+    }
+}
---
+++ ProgrammersBlog.Shared/Entities/Abstract/EntityBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Entities.Abstract
+{
+    public abstract class EntityBase    //Abstract vermemizin nedeni diğer sınıflarda değişikliğe uğrayacak olmasıdır.
+    {
+        public virtual int Id { get; set; }
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now; //override CreatedDate = new DateTime(2021/01/01)
+        public virtual DateTime ModifiedDate { get; set; } = DateTime.Now;
+        public virtual bool IsDeleted { get; set; } = false;
+        public virtual bool IsActive { get; set; } = true;
+        public virtual string CreatedByName { get; set; }
+        public virtual string ModifiedbyName { get; set; }
+    }
+}
