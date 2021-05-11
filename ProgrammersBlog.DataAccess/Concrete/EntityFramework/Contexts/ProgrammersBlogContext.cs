1e3617041d8f43ff02dca65d9406e65cb5f0d2e0
Ahmet İhsan <ahmetihsan24@gmail.com>
2021-05-11 12:07:45 +0300

    Data Access Katmanının Oluşt.

---
+++ ProgrammersBlog.DataAccess/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.DataAccess.Concrete.EntityFramework.Contexts
+{
+    public  class ProgrammersBlogContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\MSSQLLocalDB;Database=ProgrammersBlogDb;integrated security=true;");
+        }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Role> Role { get; set; }
+        public DbSet<User> Users { get; set; }
+
+
+    }
+}
---
+++ ProgrammersBlog.Shared/Data/Abstract/IEntityRepository.cs
@@ -0,0 +1,31 @@
+﻿using ProgrammersBlog.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Data.Abstract
+{
+    public interface IEntityRepository<T> where T:class,IEntity ,new()
+        //Generic olan gönd class ın kontrolü örn. abstract class gönd zaman metodlar calışmaz IEntity ise Db de ki nesnelerimiz için ve de new yapılabilmeli
+    {
+        //Başka projelerde de kullanac. Generic Repository Async olab. olmayab. sadece projeye özgü
+        Task<T> GetAsync(Expression<Func<T,bool>> predicate,params Expression<Func<T,object>>[] includeProperties); // var kullanıcı = repository.GetAsync(k=>k.id==15) 
+        // params Expression<Func<T,object>>[] includeProperties 25 id'li bir makaleyi getirirken makale ile birlikte kullanıcıyı ve yorumlarıda include etmek istiyoruz
+        // ThenInclude gibi...
+
+        Task<IList<T>> GetAllAsync(Expression<Func<T, bool>> predicate=null, params Expression<Func<T, object>>[] includeProperties);
+        //Tüm kategorileri getirmek istiyoruz reporsitory.GetAllAsync(y=>y.ArticleID==1);
+
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(T entity);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> predicate);
+        // var result = _userReporsitory.AnyAsync(u=>u.FirstName=="Ali"); var mı 
+        Task<int> CountAsync(Expression<Func<T, bool>> predicate);
+
+    
+    }
+}
