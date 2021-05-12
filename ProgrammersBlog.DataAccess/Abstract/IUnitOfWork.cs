77b0aab1838a180bd01ecc7202a18616c6ce6425
Ahmet İhsan <ahmetihsan24@gmail.com>
2021-05-12 11:04:03 +0300

    IUnitOfWork Arayuzunun oluşt.

---
+++ ProgrammersBlog.DataAccess/Abstract/IUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.DataAccess.Abstract
+{
+     public interface IUnitOfWork:IDisposable
+    {
+        //Unit of Work sayesinde tüm repositorileri aynı yerden yönet.
+        //Örn. _articleRepository.AddAsync(article)
+        //     _articleRepository.SaveAsync()  vb. tüm repoları yeniden yazmak yerine yerine
+        // _unitofwork.Article.AddAsync(article) 
+        //_unitofwork.User.AddAsync(user) 
+        //
+        IArticleRepository Articles { get; } //
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; }
+        Task<int> SaveAsync(); //Db ye kaydedilecek olan Transaction sayısı..
+    }
+}
