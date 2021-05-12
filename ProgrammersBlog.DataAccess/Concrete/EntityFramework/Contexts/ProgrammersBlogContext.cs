df37ec01c52cf3476743c7d56a21314685321984
Ahmet İhsan <ahmetihsan24@gmail.com>
2021-05-12 10:28:18 +0300

    Db icin Mapping islemleri

--- ProgrammersBlog.DataAccess/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
+++ ProgrammersBlog.DataAccess/Concrete/EntityFramework/Contexts/ProgrammersBlogContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProgrammersBlog.DataAccess.Concrete.EntityFramework.Mappings;
 using ProgrammersBlog.Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -13,7 +14,8 @@ namespace ProgrammersBlog.DataAccess.Concrete.EntityFramework.Contexts
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\MSSQLLocalDB;Database=ProgrammersBlogDb;integrated security=true;");
+            optionsBuilder.UseSqlServer(connectionString: @"Server=(localdb)\MSSQLLocalDB;Database=ProgrammersBlogDb;Trusted_Connection=True;
+                                                            Timeout=30;MultipleActiveResultSets=True;");
         }
         public DbSet<Article> Articles { get; set; }
         public DbSet<Category> Categories { get; set; }
@@ -21,6 +23,14 @@ namespace ProgrammersBlog.DataAccess.Concrete.EntityFramework.Contexts
         public DbSet<Role> Role { get; set; }
         public DbSet<User> Users { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder) //Db oluş zaman uygulanacak Mapping işlem. ayarlıyoruz..,Db-First ile de Kullanılab.
+        {
+            modelBuilder.ApplyConfiguration(new ArticleMap());
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new CategoryMap());
+            modelBuilder.ApplyConfiguration(new RoleMap());
+            modelBuilder.ApplyConfiguration(new UserMap());
+        }
 
     }
 }
