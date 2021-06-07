f3b041d20c0665e707df120d65b9117daade49d7
Ahmet İhsan <ahmetihsan24@gmail.com>
2021-06-07 17:32:55 +0300

    Article ve Category Servis katmanı oluşt.

---
+++ ProgrammersBlog.Entities/Dtos/CategoryUpdateDto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Dtos
+{
+   public class CategoryUpdateDto
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Kategori Adı")]
+        [Required(ErrorMessage = "{0} Boş geçilmemelidir")]
+        [MaxLength(70, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
+        public string Name { get; set; }
+
+        [DisplayName("Kategori Açıklaması")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
+        public string Description { get; set; }
+
+        [DisplayName("Kategori Özel Not Açıklaması")]
+        [MaxLength(500, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} karakterden az olmamalıdır.")]
+        public string Note { get; set; }
+
+
+
+        [DisplayName("Aktif mi?")]
+        [Required(ErrorMessage = "{0} Boş geçilmemelidir")]
+        public bool IsActive { get; set; }
+
+
+        [DisplayName("Silinsin mi?")]
+        [Required(ErrorMessage = "{0} Boş geçilmemelidir")]
+        public bool IsDeleted { get; set; }
+    }
+}
---
+++ ProgrammersBlog.Services/Abstract/ICategoryService.cs
@@ -0,0 +1,22 @@
+﻿using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Entities.Dtos;
+using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Services.Abstract
+{
+   public  interface ICategoryService
+    {
+        Task<IDataResult<Category>> Get(int categoryId);
+        Task<IDataResult<IList<Category>>> GetAll();
+        Task<IDataResult<IList<Category>>> GetAllByNonDeleted();
+        Task<IResult> Add(CategoryAddDto categoryAddDto , string createdByName);
+        Task<IResult> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName);
+        Task<IResult> Delete(int categoryId, string modifiedByName); // sadece frontend taraf. kullanıcıya silind. göst. için
+        Task<IResult> HardDelete(int categoryId); //db den silmek için...
+    }
+}
