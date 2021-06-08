f5b402c433b7e637ba7c6dee63bd7a1d8856fbea
Ahmet İhsan <ahmetihsan24@gmail.com>
2021-06-08 12:41:01 +0300

    Admin Area icin _Layout dosyası islemleri yapıl.

--- ProgrammersBlog.WebUI/Startup.cs
+++ ProgrammersBlog.WebUI/Startup.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using ProgrammersBlog.Services.Extensions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -32,6 +34,12 @@ namespace ProgrammersBlog.WebUI
             }
 
             app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+          FileProvider = new PhysicalFileProvider(
+         Path.Combine(env.ContentRootPath, "MyStaticFiles")),
+                RequestPath = "/StaticFiles"
+            });
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
