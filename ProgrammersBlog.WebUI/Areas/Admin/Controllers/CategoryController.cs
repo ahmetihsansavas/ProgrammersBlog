bc3b4a1682e41dac63034bff91db05485a713a3e
Ahmet İhsan <ahmetihsan24@gmail.com>
2021-06-10 22:32:49 +0300

    CategoryController/Add Metodu hazırlanması

--- ProgrammersBlog.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ ProgrammersBlog.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammersBlog.Entities.Dtos;
 using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Shared.Utilities.Extensions;
 using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using ProgrammersBlog.WebUI.Areas.Admin.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,8 +32,19 @@ namespace ProgrammersBlog.WebUI.Areas.Admin.Controllers
             
         }
 
+        [HttpGet]
         public IActionResult Add() 
         {
+            return PartialView("_CategoryAddPartial");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(CategoryAddDto categoryAddDto)
+        {
+            var categoryAjaxModel = new CategoryAddAjaxViewModel()
+            {
+                CategoryAddPartial = await this.RenderViewToStringAsync("_CategoryAddPartial",categoryAddDto), //Projemize dısarıdan ekled. ControllerExtensions sınıfımızdan cektig. metod bu metod sayesinde partial view e json olarak veri aktarımı yap.
+                
+            };
             return PartialView("_CategoryAddPartial");
         }
     }
