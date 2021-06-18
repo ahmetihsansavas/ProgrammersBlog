9bec76edbdb91e5a1eda101c22d7853c5cc95892
Ahmet İhsan <ahmetihsan24@gmail.com>
2021-06-18 21:31:51 +0300

    ServiceCollecExtens. içeris. User password ve Username ayarları

--- ProgrammersBlog.Services/Extensions/ServiceCollectionExtensions.cs
+++ ProgrammersBlog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -19,7 +19,21 @@ namespace ProgrammersBlog.Services.Extensions
         {
             //WebUI kısmında Startup.cs içeris.de de yap. ancak biz düzenli olmasını isted. için yaptık.
             serviceCollection.AddDbContext<ProgrammersBlogContext>();
-            serviceCollection.AddIdentity<User, Role>().AddEntityFrameworkStores<ProgrammersBlogContext>();
+            //Identity i uyg. tanıtıyoruz ve gerekli ayarlamaları yap.
+            serviceCollection.AddIdentity<User, Role>(options => 
+            {
+                //User password ayarları
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequiredUniqueChars = 0; //kull. sifreye özel karak. sayısı iki tane @@ eklersek
+                options.Password.RequireNonAlphanumeric = false; // !,? 
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+
+                //User Username and Email Options
+                options.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;
+            }).AddEntityFrameworkStores<ProgrammersBlogContext>();
             serviceCollection.AddScoped<IUnitOfWork, UnitOfWork>();
             serviceCollection.AddScoped<ICategoryService, CategoryManager>();
             serviceCollection.AddScoped<IArticleService, ArticleManager>();
