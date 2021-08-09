bdf6a09d939ac54851d5c763e7d78377aacabb2e
Ahmet İhsan <ahmetihsan24@gmail.com>
2021-08-09 17:16:46 +0300

    User PasswordChange Metod. ve Sayfası Olust.

--- ProgrammersBlog.WebUI/Areas/Admin/Controllers/UserController.cs
+++ ProgrammersBlog.WebUI/Areas/Admin/Controllers/UserController.cs
@@ -346,7 +346,13 @@ namespace ProgrammersBlog.WebUI.Areas.Admin.Controllers
             }
         }
 
-
+        [Authorize]
+        [HttpGet]
+        public ViewResult PasswordChange() 
+        {
+            return View();  
+        
+        }
 
         [Authorize(Roles = "Admin,Editor")]
         public async Task<string> ImageUpload(string userName,IFormFile pictureFile) 
