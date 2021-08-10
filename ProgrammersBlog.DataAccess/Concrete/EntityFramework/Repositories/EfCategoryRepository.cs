cf72904e2a1a5161ef630d72fd4a4a2674898525
Ahmet İhsan <ahmetihsan24@gmail.com>
2021-08-10 13:01:39 +0300

    EfRepository ve User Index refactor islem.

--- ProgrammersBlog.DataAccess/Concrete/EntityFramework/Repositories/EfCategoryRepository.cs
+++ ProgrammersBlog.DataAccess/Concrete/EntityFramework/Repositories/EfCategoryRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProgrammersBlog.DataAccess.Abstract;
+using ProgrammersBlog.DataAccess.Concrete.EntityFramework.Contexts;
 using ProgrammersBlog.Entities.Concrete;
 using ProgrammersBlog.Shared.Data.Concrete.EntityFramework;
 using System;
@@ -12,9 +13,17 @@ namespace ProgrammersBlog.DataAccess.Concrete.EntityFramework.Repositories
 {
     public class EfCategoryRepository : EfEntityRepositoryBase<Category>,ICategoryRepository
     {
+        private ProgrammersBlogContext ProgrammersBlogContext { get { return _context as ProgrammersBlogContext; } }
         public EfCategoryRepository(DbContext context) : base(context)
         {
 
         }
+
+        public async Task<Category> GetById(int categoryId)
+        {
+            return await ProgrammersBlogContext.Categories.SingleOrDefaultAsync(c => c.Id == categoryId);
+
+        }
+
     }
 }
