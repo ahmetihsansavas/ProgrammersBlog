5d06125f3f842488f7d38b0a589675cbb8b364af
Ahmet İhsan <ahmetihsan24@gmail.com>
2021-08-17 15:54:11 +0300

    Article Cont. Olust.

---
+++ ProgrammersBlog.WebUI/Areas/Admin/Controllers/ArticleController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ArticleController : Controller
+    {
+        private readonly IArticleService _articleService;
+
+        public ArticleController(IArticleService articleService)
+        {
+            _articleService = articleService;
+        }
+
+        [HttpGet]
+        public async Task <IActionResult> Index()
+        {
+            var result= await _articleService.GetAllByNonDeleted();
+            if (result.ResultStatus==ResultStatus.Success)
+            {
+                return View(result.Data);
+            }
+
+            return NotFound();
+        }
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            return View();
+        }
+    }
+}
