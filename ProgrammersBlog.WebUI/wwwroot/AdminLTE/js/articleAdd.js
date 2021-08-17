324785142ea68160dfb063cb18ad75e5adf27a86
Ahmet İhsan <ahmetihsan24@gmail.com>
2021-08-17 16:33:43 +0300

    Article Add View ve articleAdd.js olust.

---
+++ ProgrammersBlog.WebUI/wwwroot/AdminLTE/js/articleAdd.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $('#text-editor').trumbowyg({
+        lang: 'tr',
+        btns: [
+            ['viewHTML'],
+            ['undo', 'redo'], // Only supported in Blink browsers
+            ['formatting'],
+            ['strong', 'em', 'del'],
+            ['superscript', 'subscript'],
+            ['link'],
+            ['insertImage'],
+            ['justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull'],
+            ['unorderedList', 'orderedList'],
+            ['horizontalRule'],
+            ['removeformat'],
+            ['fullscreen'],
+            ['foreColor', 'backColor'],
+            ['emoji'],
+            ['fontfamily'],
+            ['fontsize']
+        ]
+    });
+});
